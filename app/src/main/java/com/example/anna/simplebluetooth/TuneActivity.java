ad602819d21554ac595abf95b183b46d0d81bb25
Anna <anna.proprentner@htl-klu.at>
2018-12-05 15:43:49 +0100

    TuneActivity Added SendString to ESP Get Bluetooth Connection / Devices

---
+++ app/src/main/java/com/example/anna/simplebluetooth/TuneActivity.java
@@ -0,0 +1,12 @@
+package com.example.anna.simplebluetooth;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+public class TuneActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
