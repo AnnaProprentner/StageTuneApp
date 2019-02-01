0aa609a554bc6277fc067e4f7aa09cad2c218310
Anna <anna.proprentner@htl-klu.at>
2019-02-01 08:28:39 +0100

    PopUp Window created -No PopUpActivity -Open on btn Clicked -Close if clicked anywhere

--- app/src/main/java/com/example/anna/simplebluetooth/NowTuningActivity.java
+++ app/src/main/java/com/example/anna/simplebluetooth/NowTuningActivity.java
@@ -1,5 +1,6 @@
 package com.example.anna.simplebluetooth;
 
+import android.annotation.SuppressLint;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothSocket;
@@ -8,10 +9,15 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.PopupWindow;
 import android.widget.Spinner;
 import android.widget.Toast;
 
@@ -43,6 +49,13 @@ public class NowTuningActivity extends AppCompatActivity {
     EditText edtReceive;
     Button btnStart;
     Spinner spStringSelect;
+    String selectedString;
+
+    Button btnclose;
+
+    String receivedString;
+
+    String[] Tune = new String[7];
 
     String message;
 
@@ -50,6 +63,14 @@ public class NowTuningActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_now_tuning);
+
+        Tune[1] = "E";
+        Tune[2] = "A";
+        Tune[3] = "D";
+        Tune[4] = "G";
+        Tune[5] = "H";
+        Tune[6] = "e";
+
 
         edtSend = (EditText) findViewById(R.id.edtSend);
         edtReceive = (EditText) findViewById(R.id.edtReceive);
@@ -68,15 +89,79 @@ public class NowTuningActivity extends AppCompatActivity {
 
         btnStart.setOnClickListener(new View.OnClickListener(){
 
+
             @Override
             public void onClick(View view) {
               message = edtSend.getText().toString();
-                Toast.makeText(getApplicationContext(), message, Toast.LENGTH_LONG).show();
+              selectedString = spStringSelect.getSelectedItem().toString();
 
-                SendString(message);
+              if(selectedString.equals("E")){
+                  SendString("E;");
+              }else if(selectedString.equals("A")){
+                  SendString("A;");
+              }else if(selectedString.equals("D")){
+                  SendString("D;");
+              }else if(selectedString.equals("G")){
+                  SendString("G;");
+              }else if(selectedString.equals("H")){
+                  SendString("H;");
+              }else if(selectedString.equals("e")){
+                  SendString("e;");
+              }
+
+              showPopupWindow(view);
+              //view.setForeground().setAlpha(220);
+
+
+             /* switch (selectedString){
+                  case "E": SendString("E;");
+                      Toast.makeText(getApplicationContext(), selectedString, Toast.LENGTH_LONG).show();
+                  case "A": SendString("A;");
+                  case "D": SendString("D;");
+                  case "G": SendString("G;");
+                  case "H": SendString("H;");
+                  case "e": SendString("e;");
+              }*/
+
+               // SendString(message);
             }
         });
 
+    }
+
+
+    public void showPopupWindow(View view){
+        LayoutInflater inflater = (LayoutInflater) getSystemService(LAYOUT_INFLATER_SERVICE);
+        View popupView = inflater.inflate(R.layout.popup_window, null);
+
+        //TO DO: btnclose = (Button) findViewById(R.layout.btn)
+
+        int width = LinearLayout.LayoutParams.WRAP_CONTENT;
+        int height = LinearLayout.LayoutParams.WRAP_CONTENT;
+
+        boolean focus = true;
+
+        final PopupWindow popupWindow = new PopupWindow(popupView,width,height,focus);
+
+
+
+        //popupView.getForeground().setAlpha(0);
+        popupWindow.showAtLocation(view, Gravity.CENTER,0,0);
+
+
+        if(receivedString.equals("close0")){
+            popupWindow.dismiss();
+        }
+
+/*        // dismiss the popup window when touched
+        popupView.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                popupWindow.dismiss();
+                return true;
+            }
+        });
+*/
     }
 
     public void ConnectESP(){
@@ -173,6 +258,8 @@ public class NowTuningActivity extends AppCompatActivity {
         readBufferPosition = 0;
         readBuffer = new byte[1024];
 
+        final String[] receivedDataBT = new String[1];
+
         workerThread = new Thread(new Runnable() {
             @Override
             public void run() {
@@ -188,6 +275,7 @@ public class NowTuningActivity extends AppCompatActivity {
 
                             byte[] packetBytes = new byte[bytesAvailable];
                             myInputStream.read(packetBytes); //LOOK UP
+
                             for (int i = 0; i<bytesAvailable;i++){
                                 byte b = packetBytes[i];
 
@@ -195,15 +283,19 @@ public class NowTuningActivity extends AppCompatActivity {
                                     byte[] encodedBytes = new byte[readBufferPosition];
                                     System.arraycopy(readBuffer,0,encodedBytes,0,encodedBytes.length); //LOOK UP
                                     final String data = new String(encodedBytes,"US-ASCII");
+
+                                    receivedString = data;
+
                                     readBufferPosition = 0;
+
 
                                     handler.post(new Runnable() {
                                         @Override
                                         public void run() {
                                             //edtMessage.setText(data);
                                             Toast.makeText(getApplicationContext(), data, Toast.LENGTH_SHORT).show();
-
                                             edtReceive.setText(data);
+
 
                                         }
                                     });
@@ -227,6 +319,7 @@ public class NowTuningActivity extends AppCompatActivity {
 
         workerThread.start();
 
+       // return receivedDataBT[1];
     }
 
 }
